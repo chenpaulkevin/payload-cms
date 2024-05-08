1da627c029564e11dc4b3f59764b6a1f94ea03de
Paul Chen <47184835+chenpaulkevin@users.noreply.github.com>
2024-05-08 09:52:28 +0800

    Node Mailer: Node Mailer
    - Add Node mailer mock mailer

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import payload from 'payload'
 import path from 'path';
+import nodemailer from 'nodemailer';
 
 require('dotenv').config()
 const app = express()
 app.use('/assets', express.static(path.resolve(__dirname, './assets')));
+
+
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
@@ -12,12 +15,34 @@ app.get('/', (_, res) => {
 })
 
 const start = async () => {
+
+  const nodemailer = require('nodemailer');
+  const transport = await nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  })
+  
   // Initialize Payload
   await payload.init({
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.sendEmail({
+        to: 'gbv7une2bjqyk5uh@ethereal.email',
+        subject: 'Hello',
+        text: 'Initial Email',
+      })
+    },
+    email: {
+      fromName: 'Admin',
+      fromAddress: 'admin@example.com',
+      logMockCredentials: true, // Optional
+      transport,
     },
   })
 
